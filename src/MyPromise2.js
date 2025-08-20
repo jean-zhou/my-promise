6f98a46f99bb14087e5e337dc253e0ac4cb5a1fa
jean-zhou <7774846+jean-zhou@user.noreply.gitee.com>
2025-08-20 22:11:34 +0800

    finish test 2.2.2

--- src/MyPromise2.js
+++ src/MyPromise2.js
@@ -5,28 +5,54 @@ class Promise {
   status = PENDING;
   value = null;
   reason = undefined;
+  fulfillCallbacks = [];
+  rejectCallbacks = []
 
   constructor(exector) {
     const resolve = (value) => {
       if (this.status === PENDING) {
         this.status = FULFILLED;
         this.value = value;
+        console.log('resolve func ----', this.fulfillCallbacks);
+        // this.fulfillCallbacks.forEach((onFulfilled) => onFulfilled(this.value))
+        this.fulfillCallbacks.forEach((onFulfilled) => setTimeout(() => {
+          onFulfilled(this.value)
+        }))
       }
     };
     const reject = (reason) => {
       if (this.status === PENDING) {
         this.status = REJECTED;
         this.reason = reason;
+        // this.rejectCallbacks.forEach((onRejected) => onRejected(this.reason))
+        this.rejectCallbacks.forEach((onRejected) => setTimeout(() => {
+          onRejected(this.reason)
+        }))
       }
     };
     exector(resolve, reject);
   }
   then(onFulfilled, onRejected) {
+    console.log('then onFulfilled', onFulfilled);
+    console.log('then onRejected', onRejected);
+    if (this.status === PENDING
+      && typeof onFulfilled === 'function'
+      || typeof onRejected === 'function'
+    ) {
+      console.log('then pending ----')
+      this.fulfillCallbacks.push(onFulfilled);
+      this.rejectCallbacks.push(onRejected)
+    }
+    console.log('this.status ---', this.status)
     if (this.status === FULFILLED && typeof onFulfilled === 'function') {
-      onFulfilled();
+      setTimeout(() => {
+        onFulfilled(this.value);
+      })
     }
     if (this.status === REJECTED && typeof onRejected === 'function') {
-      onRejected();
+      setTimeout(() => {
+        onRejected(this.reason);
+      })
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 // const Promise = require('./MyPromise')
 const Promise = require('./MyPromise2');
-
+let func = null;
 const p = new Promise((resolve, reject) => {
   // throw 'error---';
-  resolve(100);
+  // resolve(100);
   // reject('失败--');
   // setTimeout(() => {
   //   resolve(100);
   // }, 100)
-}).then(
+  console.log(' promise ---')
+  func = resolve;
+  // func = reject;
+})
+
+p.then(
   (value) => {
     console.log('then 成功', value);
-    return p;
+    // return p;
     // return new Promise((resolve, reject) => {
     //   console.log('第二个 promise');
     //   resolve(new Promise((resolve, reject) => {
@@ -24,6 +29,8 @@ const p = new Promise((resolve, reject) => {
     console.log('then 失败', reason);
   }
 )
+// func(100);
+func('reject');
 // .then((value) => {
 //   console.log('第二个 then 成功', value);
 // }, (reason) => {
